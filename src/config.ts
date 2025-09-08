6a32da40319477b9e94113a1709afa4ace0b38e0
R4finh4sz <rafael.souza6657@gmail.com>
2025-09-07 22:04:49 -0300

    Feat: Added new interfaces

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,6 @@ import 'dotenv/config';
 export const config = {
   token: process.env.DISCORD_TOKEN,
   clientId: process.env.CLIENT_ID,
+  guildId: process.env.GUILD_ID,
+  ownerId: process.env.BOT_OWNER_ID,
 };
